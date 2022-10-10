97bfcb6a868a990cd004d7c0b21e268b9f649123
girithc <girithmchoudhary@gmail.com>
2022-10-10 09:15:30 -0500

    add to testParserAndInterpreter.cpp

--- lox/testParserAndInterpreter.cpp
+++ lox/testParserAndInterpreter.cpp
@@ -1,3 +1,54 @@
+#include "scanner.cpp"
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <iterator>
+#include <fstream>
+
+using namespace std;
+
+/*
+var i = 1;
+  for (var a = 0; i < 3; a=a+1) {
+    i =a;
+    print a;
+  }
+  
+  print i;
+
+var a = 1;
+while(a > 0)
+{
+  a = i;
+  i = i -1;
+  print a;
+
+}
+
+if ( a < 1) print "a is less than 1: a =" + a;
+else print "a is greater than 1";
+*/
+
+void testIfStatement()
+{
+    ofstream testFile;
+    testFile.open("test/testParserAndScannerResult.txt");
+    testFile.close();
+}
+
+void testWhileStatement()
+{
+    ofstream testFile;
+    testFile.open("test/testParserAndScannerResult.txt", ios_base::app);
+    testFile.close();
+}
+
+void testForStatement()
+{
+    ofstream testFile;
+    testFile.open("test/testParserAndScannerResult.txt", ios_base::app);
+    testFile.close();
+}
 
 
 
@@ -5,7 +56,7 @@
 void testParserAndInterpreter()
 {
     cout << "Enter testParserAndInterpreter()" << endl;
-    testIfStatment();
+    testIfStatement();
     testWhileStatement();
     testForStatement();
 }
